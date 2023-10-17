4d9167e6914641ee008bb63d3943b51b99d95e31
ShaughnAnderson94 <andersji94@gmail.com>
2023-10-17 11:49:49 +0100

    added alive state

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,37 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
-import Sleep from './Sleep.jsx'
-import Coding from './Coding.jsx'
-import Coffee from './Coffee.jsx'
+import "./App.css";
+import Sleep from "./Sleep.jsx";
+import Coding from "./Coding.jsx";
+import Coffee from "./Coffee.jsx";
 
 function App() {
-  const [energy, setEnergy] = useState(50)
-  const [codingSkill, setCodingSkill]= useState(0)
+  const [energy, setEnergy] = useState(50);
+  const [codingSkill, setCodingSkill] = useState(0);
+  const [alive, setAlive] = useState(true);
 
   return (
     <>
-      <div>
-
-      </div>
+      <div></div>
       <h1>Pet Programmer</h1>
       <div className="card">
-        <Sleep setEnergy={setEnergy}/>
-        <Coding energy={energy} setEnergy= {setEnergy} codingSkill = {codingSkill} setCodingSkill = {setCodingSkill} />
-        <Coffee setEnergy={setEnergy}/>
+        <Sleep setEnergy={setEnergy} />
+        <Coding
+          energy={energy}
+          setEnergy={setEnergy}
+          codingSkill={codingSkill}
+          setCodingSkill={setCodingSkill}
+          setAlive={setAlive}
+          alive={alive}
+        />
+        <Coffee setEnergy={setEnergy} />
       </div>
       <div className="stats">
-        <p>
-          energy level is {energy}
-        </p>
-        <p>
-          coding level is {codingSkill}
-        </p>
+        <p>energy level is {energy}</p>
+        <p>coding level is {codingSkill}</p>
       </div>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Coding.jsx
+++ src/Coding.jsx
@@ -1,10 +1,14 @@
-
-export default function Coding(props){
-return <button onClick={() =>{
-          props.setCodingSkill((codingSkill) => codingSkill + 1)
-          props.setEnergy((energy) => energy - 20)
-        }
-        }>
-          Code
-        </button>
+export default function Coding(props) {
+  console.log(props.alive);
+  return (
+    <button
+      onClick={() => {
+        props.setCodingSkill(props.codingSkill + 1);
+        props.setEnergy(props.energy - 20);
+        props.energy - 20 < 0 && props.setAlive(false);
+      }}
+    >
+      Code
+    </button>
+  );
 }
