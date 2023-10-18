c421c9d62760bdbcf40c94af56e4dd641acab3c7
ShaughnAnderson94 <andersji94@gmail.com>
2023-10-18 13:02:20 +0100

    age work

--- src/utils/aged.js
+++ src/utils/aged.js
@@ -1,7 +1,7 @@
 import timePasses from "./timePasses";
 
 export default function aged(dispatch, state) {
-  dispatch({ type: "update", payload: { age: timePasses() } });
+  dispatch({ type: "update", payload: { age: timePasses(dispatch, state) } });
   dispatch({ type: "update", payload: { energy: state.energy - 3 } });
   dispatch({ type: "update", payload: { rounds: 0 } });
 }
