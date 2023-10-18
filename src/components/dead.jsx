6c3a7725f3b42bd3a5f6da0c92efa6f923182d06
Yuqing Wang <44486576+yuqingwwang@users.noreply.github.com>
2023-10-18 16:51:32 +0100

    done

--- src/components/dead.jsx
+++ src/components/dead.jsx
@@ -1,6 +1,7 @@
 import useWindowSize from 'react-use/lib/useWindowSize'
 import Confetti from 'react-confetti'
 
+// Tom made us do this
 export default function Dead ({state}) {
   const { width, height } = useWindowSize()
 
