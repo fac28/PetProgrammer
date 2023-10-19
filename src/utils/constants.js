6f03cd9cc5e4ea5f38231ad0280f39f5f46784dc
Yuqing Wang <44486576+yuqingwwang@users.noreply.github.com>
2023-10-19 10:20:57 +0100

    removed comments

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,14 +1,3 @@
-// export const imagePaths = {
-//   coffee:
-//     "https://s-media-cache-ak0.pinimg.com/736x/da/16/f0/da16f0202c2cb4973f699b1e362b34e8.jpg",
-//   coding:
-//     "https://tse2.mm.bing.net/th/id/OIP.2SEbYMe4RrMhSEmOikmfRQHaJ4?w=130&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
-//   sleep:
-//     "https://i.pinimg.com/736x/16/20/ee/1620ee900a6adc4a482cf250ac8bee87--snoopy-peanuts.jpg",
-//   death:
-//     "https://tse2.mm.bing.net/th/id/OIP.g5L0x4dJMsx2g7xWuAiWaAHaHa?w=205&h=205&c=7&r=0&o=5&dpr=1.3&pid=1.7",
-// };
-
 export const imagePaths = {
   coffee:
     "./coffee.jpg",
